3658914369871ba01298fb63b59286bfff1ae5b7
SOFIYA BABY <sofiyababy111@gmail.com>
2024-12-17 16:41:48 +0530

    Added field names to show.blade.php

--- resources/views/crud_app_db/show.blade.php
+++ resources/views/crud_app_db/show.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="container mt-5">
-    <h2>{{ $crud_app_db->name }}</h2>
-    <p>{{ $crud_app_db->age }}</p>
-    <p>{{ $crud_app_db->email }}</p>
+    <h2>Name : {{ $crud_app_db->name }}</h2>
+    <p>Age : {{ $crud_app_db->age }}</p>
+    <p>Email : {{ $crud_app_db->email }}</p>
     <a href="{{ route('crud_app_db.index') }}" class="btn btn-secondary">Back</a>
 </div>
 @endsection
